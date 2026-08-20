af2e549e81643a37e554ab0b4f40b2d7a5eb3c49
core-999 <devcores999@gmail.com>
2026-08-20 19:45:14 +0630

    Dev999

--- bot1.py
+++ bot1.py
@@ -230,11 +230,11 @@ def main_buttons():
             url=f"https://t.me/{BOT_USERNAME}?startgroup=s&delete_message+manage_video_chats_message+invite_users")
           )
     kb.row(    
-         InlineKeyboardButton("DEV", url="https://t.me/HANTHAR_1999"),
+         InlineKeyboardButton("DEV", url="https://t.me/cores_999"),
          InlineKeyboardButton(" စကားပြော ", url="https://t.me/myanmar_music_Bot2027")
           )
     kb.row(  
-        InlineKeyboardButton("Botပြုလုပ်လိုပါက", url="tg://resolve?domain=HEX_KING9&text=Botအသစ်လုပ်ချင်လို့ပါ"),
+        InlineKeyboardButton("Botပြုလုပ်လိုပါက", url="tg://resolve?domain=cores_999&text=Botအသစ်လုပ်ချင်လို့ပါ"),
         InlineKeyboardButton("UPDATE", url="https://t.me/myanmarbot_music")
           )
     return kb
